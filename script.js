eb8f082318de3c4b9af780fd5e4c6524c223239a
salteddev <68326271+salteddev@users.noreply.github.com>
2020-09-23 17:57:58 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function showNumbers() {
+    document.getElementById("more_numbers").classList.toggle("show");
+    document.getElementById("chevron").classList.toggle("rotate");
+}
+function showNumbers2() {
+    document.getElementById("more_numbers2").classList.toggle("show");
+    document.getElementById("chevron2").classList.toggle("rotate");
+}
+function showSideMenu(){
+    document.getElementById("side_menu").classList.toggle("open")
+    document.getElementById("side_list").classList.toggle("open_list")
+    document.getElementById("burger").classList.toggle("is-active")
+    
+}
+
